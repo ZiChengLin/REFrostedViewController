da3ef10406725a9451b7054725f58d2c5f3f8f09
ZiChengLin <260220408@qq.com>
2015-06-18 20:44:53 +0800

    Initial Commit

---
+++ REFrostedViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  REFrostedViewController
+//
+//  Created by 林梓成 on 15/6/18.
+//  Copyright (c) 2015年 lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
